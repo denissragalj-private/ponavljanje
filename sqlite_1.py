54cf11d18c199fd09457ad20243d4299110372b9
denissragalj-private <denis.sragalj@gmail.com>
2026-02-04 19:25:20 +0100

    SQL

--- sqlite_1.py
+++ sqlite_1.py
@@ -1,29 +1,37 @@
 import sqlite3
 
-# 1 konekcija na bazu podataka (ako baza ne postoji, biće kreirana)
+
+artist_id = int(input('Upisite ID izvodaca: '))
+artist_id_end = int(input('Upisite ID izvodaca: '))
+
+
+# 1. Konekcija
 conn = sqlite3.connect('chinook.db')
 
-# 2 kreiranje kursora
-c = cursor = conn.cursor()
+# 2. Cursor
+c = conn.cursor()
 
-# 3 izvršavanje SQL upita EXECUTE
-# -- SELECT * FROM albums
-# -- SELECT * FROM albums WHERE artistid = 10
-# -- SELECT * FROM albums WHERE artistid BETWEEN 10 AND 15
+# 3. EXECUTE
+    # -- SELECT * FROM albums
+    # -- SELECT Title FROM albums
+    # -- SELECT * FROM albums WHERE artistid = 20
 
-# SELECT * FROM albums WHERE title like '%Bi%'
+    # -- SELECT * FROM albums WHERE artistid BETWEEN 10 AND 15
+    # -- SELECT * FROM albums WHERE title like 'Bi%'
+# c.execute('''SELECT * FROM albums''')
+# c.execute('''SELECT Title FROM albums''')
+# c.execute('''SELECT * FROM albums WHERE artistid = 20''')
+# c.execute('''SELECT * FROM albums WHERE artistid = ?''', (artist_id,))
+c.execute('''SELECT * FROM albums WHERE artistid BETWEEN ? AND ?''', (artist_id, artist_id_end))
 
-# -- SELECT * FROM albums WHERE artistid BETWEEN 10 AND 15
-c.execute('''SELECT * FROM albums WHERE artistid BETWEEN ? AND ?''', (10, 15))
 
-#3.1. Dohvat iz baze podataka - fetchall, fetchone, fetchmany
-# results = c.fetchall()  # dohvat svih rezultata
+# 3.1. dohvat -> FETCH
+data_from_db = c.fetchall()
+print(data_from_db)
 
-#3.2 prikaz svih rezultata
-results = c.fetchall()    
-    
-print(results)
-# 4 zatvaranje konekcije
+# 3.2. promjena u bazi -> COMMIT
+# Ako je SELECT ili dohvat, onda nema COMMIT jer ne radimo nikakve promjene
+
+# 4. CLOSE
+c.close()
 conn.close()
-# results = c.fetchone()  # dohvat jednog rezultata
-# results = c.fetchmany(3)  # dohvat prvih 3 rezultata
